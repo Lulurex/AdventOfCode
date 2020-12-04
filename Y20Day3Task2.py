43c52d9991609895dd264b682e2e44ff84c18070
Lulurex <luluhagedorn4@gmail.com>
2020-12-03 22:57:54 -0500

    Add files via upload

---
+++ Y20Day3Task2.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+# Set up read file
+f = open("inputDec3.txt", "r")
+raw = f.read().splitlines()
+list = np.asarray(raw)
+my_array = [y for y in list]   #change to ints array
+my_array = np.asarray(my_array) #create a numpy array
+#print(my_array)
+
+def convert(a, length):
+    while a >= length:
+        a = a - length
+    return a
+
+
+hitTrees = [0, 0, 0, 0, 0]
+
+dx = [1, 3, 5, 7, 1]  # change in x
+dy = [1, 1, 1, 1, 2]  # change in y
+
+lineLength = len(my_array[1])
+x = 0  # x position
+y = 0  # y position
+for slope in range(5):
+    x = 0  # x position
+    y = 0  # y position
+    for i in range(len(my_array)-1):
+        x += dx[slope]  # move to next location
+        y += dy[slope]
+
+        if x >= lineLength:  # check out of bounds
+            x = convert(x, lineLength)
+
+        if y >= len(my_array):
+            break
+        #print("iteration: " + str(i))
+        #print(my_array[y][x])
+        if '#' == my_array[y][x]:
+            hitTrees[slope] += 1
+
+    print(hitTrees)
+product = 1
+for p in range(len(hitTrees)):
+    product = product * hitTrees[p]
+print(product)
+#my array y x
+#aline = my_array[1][0]
+#print(aline)
+#print(my_array[1])
+    #Check Tree status
+
+
+
+    #print("iteration:" + str(i))
+    #print(x)
+    #print(y)
+
+
+
+
+
+
+f.close()
